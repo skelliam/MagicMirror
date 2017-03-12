059dde33ebe6ced222e6a970c98a019db67cfb02
William Skellenger <wskellenger@gmail.com>
2017-03-12 11:48:41 -0400

    newsfeed module: use jQuery Allow newsfeed to display multiple headlines

--- modules/default/newsfeed/newsfeed.js
+++ modules/default/newsfeed/newsfeed.js
@@ -9,269 +9,277 @@
 
 Module.register("newsfeed",{
 
-	// Default module config.
-	defaults: {
-		feeds: [
-			{
-				title: "New York Times",
-				url: "http://www.nytimes.com/services/xml/rss/nyt/HomePage.xml",
-				encoding: "UTF-8" //ISO-8859-1
-			}
-		],
-		showSourceTitle: true,
-		showPublishDate: true,
-		showDescription: false,
-		reloadInterval:  5 * 60 * 1000, // every 5 minutes
-		updateInterval: 10 * 1000,
-		animationSpeed: 2.5 * 1000,
-		maxNewsItems: 0, // 0 for unlimited
-		removeStartTags: "",
-		removeEndTags: "",
-		startTags: [],
-		endTags: []
+    // Default module config.
+    defaults: {
+        feeds: [
+        {
+            title: "New York Times",
+            url: "http://www.nytimes.com/services/xml/rss/nyt/HomePage.xml",
+            encoding: "UTF-8" //ISO-8859-1
+        }
+        ],
+        showSourceTitle: true,
+        showPublishDate: true,
+        showDescription: false,
+        maxHeadlinesDisplayed: 1,  // how many headlines to display per refresh
+        reloadInterval:  5 * 60 * 1000, // every 5 minutes
+        updateInterval: 10 * 1000,
+        animationSpeed: 2.5 * 1000,
+        maxNewsItems: 0, // 0 for unlimited
+        removeStartTags: "",
+        removeEndTags: "",
+        startTags: [],
+        endTags: []
 
-	},
+    },
 
-	// Define required scripts.
-	getScripts: function() {
-		return ["moment.js"];
-	},
+    // Define required scripts.
+    getScripts: function() {
+        return ["moment.js", this.file('vendor/jquery.min.js')];
+    },
 
-	// Define required translations.
-	getTranslations: function() {
-		// The translations for the defaut modules are defined in the core translation files.
-		// Therefor we can just return false. Otherwise we should have returned a dictionairy.
-		// If you're trying to build yiur own module including translations, check out the documentation.
-		return false;
-	},
+        // Define required translations.
+    getTranslations: function() {
+        // The translations for the defaut modules are defined in the core translation files.
+        // Therefor we can just return false. Otherwise we should have returned a dictionairy.
+        // If you're trying to build yiur own module including translations, check out the documentation.
+        return false;
+    },
 
-	// Define start sequence.
-	start: function() {
-		Log.info("Starting module: " + this.name);
+        // Define start sequence.
+    start: function() {
+        Log.info("Starting module: " + this.name);
 
-		// Set locale.
-		moment.locale(config.language);
+        // Set locale.
+        moment.locale(config.language);
 
-		this.newsItems = [];
-		this.loaded = false;
-		this.activeItem = 0;
+        this.newsItems = [];
+        this.loaded = false;
+        this.activeItem = 0;
 
-		this.registerFeeds();
+        this.registerFeeds();
 
-	},
+    },
 
-	// Override socket notification handler.
-	socketNotificationReceived: function(notification, payload) {
-		if (notification === "NEWS_ITEMS") {
-			this.generateFeed(payload);
+        // Override socket notification handler.
+    socketNotificationReceived: function(notification, payload) {
+        if (notification === "NEWS_ITEMS") {
+            this.generateFeed(payload);
 
-			if (!this.loaded) {
-				this.scheduleUpdateInterval();
-			}
+            if (!this.loaded) {
+                this.scheduleUpdateInterval();
+            }
 
-			this.loaded = true;
-		}
-	},
+            this.loaded = true;
+        }
+    },
 
-	// Override dom generator.
-	getDom: function() {
-		var wrapper = document.createElement("div");
+        // Override dom generator.
+    getDom: function() {
+        var wrapper = $("<div>");
 
-		if (this.config.feedUrl) {
-			wrapper.className = "small bright";
-			wrapper.innerHTML = "The configuration options for the newsfeed module have changed.<br>Please check the documentation.";
-			return wrapper;
-		}
+        if (this.config.feedUrl) {
+            wrapper.addClass("small bright");
+            wrapper.html("The configuration options for the newsfeed module have changed."
+                    + "<br>Please check the documentation.");
+            return wrapper.get(0);
+        }
 
-		if (this.activeItem >= this.newsItems.length) {
-			this.activeItem = 0;
-		}
+        if (this.activeItem >= this.newsItems.length) {
+            this.activeItem = 0;
+        }
 
-		if (this.newsItems.length > 0) {
+        /* if we have any news items at all */
+        if (this.newsItems.length > 0) {
 
-			if (this.config.showSourceTitle || this.config.showPublishDate) {
-				var sourceAndTimestamp = document.createElement("div");
-				sourceAndTimestamp.className = "light small dimmed";
+            for (var i=0; i<this.config.maxHeadlinesDisplayed; i++) {
 
-				if (this.config.showSourceTitle && this.newsItems[this.activeItem].sourceTitle !== "") {
-					sourceAndTimestamp.innerHTML = this.newsItems[this.activeItem].sourceTitle;
-				}
-				if (this.config.showSourceTitle && this.newsItems[this.activeItem].sourceTitle !== "" && this.config.showPublishDate) {
-					sourceAndTimestamp.innerHTML += ", ";
-				}
-				if (this.config.showPublishDate) {
-					sourceAndTimestamp.innerHTML += moment(new Date(this.newsItems[this.activeItem].pubdate)).fromNow();
-				}
-				if (this.config.showSourceTitle && this.newsItems[this.activeItem].sourceTitle !== "" || this.config.showPublishDate) {
-					sourceAndTimestamp.innerHTML += ":";
-				}
+                if (this.config.showSourceTitle || this.config.showPublishDate) {
+                    var sourceAndTimestamp = $("<div>");
+                    sourceAndTimestamp.addClass("light small dimmed");
+                    var titlehtml = '';
 
-				wrapper.appendChild(sourceAndTimestamp);
-			}
+                    if (this.config.showSourceTitle && this.newsItems[this.activeItem+i].sourceTitle !== "") {
+                        titlehtml = this.newsItems[this.activeItem+i].sourceTitle;
+                    }
+                    if (this.config.showSourceTitle && this.newsItems[this.activeItem+i].sourceTitle !== "" && this.config.showPublishDate) {
+                        titlehtml += ", ";
+                    }
+                    if (this.config.showPublishDate) {
+                        titlehtml += moment(new Date(this.newsItems[this.activeItem+i].pubdate)).fromNow();
+                    }
+                    if (this.config.showSourceTitle && this.newsItems[this.activeItem+i].sourceTitle !== "" || this.config.showPublishDate) {
+                        titlehtml += ":";
+                    }
 
-			//Remove selected tags from the beginning of rss feed items (title or description)
+                    sourceAndTimestamp.html(titlehtml);
+                    wrapper.append(sourceAndTimestamp.clone());
+                }
 
-			if (this.config.removeStartTags == "title" || "both") {
+                //Remove selected tags from the beginning of rss feed items (title or description)
 
-				for (f=0; f<this.config.startTags.length;f++) {
-					if (this.newsItems[this.activeItem].title.slice(0,this.config.startTags[f].length) == this.config.startTags[f]) {
-						this.newsItems[this.activeItem].title = this.newsItems[this.activeItem].title.slice(this.config.startTags[f].length,this.newsItems[this.activeItem].title.length);
-					}
-				}
+                if (this.config.removeStartTags == "title" || "both") {
 
-			}
+                    for (f=0; f<this.config.startTags.length; f++) {
+                        if (this.newsItems[this.activeItem+i].title.slice(0,this.config.startTags[f].length) == this.config.startTags[f]) {
+                            this.newsItems[this.activeItem+i].title = this.newsItems[this.activeItem+i].title.slice(this.config.startTags[f].length,this.newsItems[this.activeItem+i].title.length);
+                        }
+                    }
 
-			if (this.config.removeStartTags == "description" || "both") {
+                }
 
-				if (this.config.showDescription) {
-					for (f=0; f<this.config.startTags.length;f++) {
-						if (this.newsItems[this.activeItem].description.slice(0,this.config.startTags[f].length) == this.config.startTags[f]) {
-							this.newsItems[this.activeItem].title = this.newsItems[this.activeItem].description.slice(this.config.startTags[f].length,this.newsItems[this.activeItem].description.length);
-						}
-					}
-				}
+                if (this.config.removeStartTags == "description" || "both") {
 
-			}
+                    if (this.config.showDescription) {
+                        for (f=0; f<this.config.startTags.length;f++) {
+                            if (this.newsItems[this.activeItem+i].description.slice(0,this.config.startTags[f].length) == this.config.startTags[f]) {
+                                this.newsItems[this.activeItem+i].title = this.newsItems[this.activeItem+i].description.slice(this.config.startTags[f].length,this.newsItems[this.activeItem+i].description.length);
+                            }
+                        }
+                    }
 
-			//Remove selected tags from the end of rss feed items (title or description)
+                }
 
-			if (this.config.removeEndTags) {
-				for (f=0; f<this.config.endTags.length;f++) {
-					if (this.newsItems[this.activeItem].title.slice(-this.config.endTags[f].length)==this.config.endTags[f]) {
-						this.newsItems[this.activeItem].title = this.newsItems[this.activeItem].title.slice(0,-this.config.endTags[f].length);
-					}
-				}
+                //Remove selected tags from the end of rss feed items (title or description)
 
-				if (this.config.showDescription) {
-					for (f=0; f<this.config.endTags.length;f++) {
-						if (this.newsItems[this.activeItem].description.slice(-this.config.endTags[f].length)==this.config.endTags[f]) {
-							this.newsItems[this.activeItem].description = this.newsItems[this.activeItem].description.slice(0,-this.config.endTags[f].length);
-						}
-					}
-				}
+                if (this.config.removeEndTags) {
+                    for (f=0; f<this.config.endTags.length;f++) {
+                        if (this.newsItems[this.activeItem+i].title.slice(-this.config.endTags[f].length)==this.config.endTags[f]) {
+                            this.newsItems[this.activeItem+i].title = this.newsItems[this.activeItem+i].title.slice(0,-this.config.endTags[f].length);
+                        }
+                    }
 
-			}
+                    if (this.config.showDescription) {
+                        for (f=0; f<this.config.endTags.length;f++) {
+                            if (this.newsItems[this.activeItem+i].description.slice(-this.config.endTags[f].length)==this.config.endTags[f]) {
+                                this.newsItems[this.activeItem+i].description = this.newsItems[this.activeItem+i].description.slice(0,-this.config.endTags[f].length);
+                            }
+                        }
+                    }
 
-			var title = document.createElement("div");
-			title.className = "bright medium light";
-			title.innerHTML = this.newsItems[this.activeItem].title;
-			wrapper.appendChild(title);
+                }
 
-			if (this.config.showDescription) {
-				var description = document.createElement("div");
-				description.className = "small light";
-				description.innerHTML = this.newsItems[this.activeItem].description;
-				wrapper.appendChild(description);
-			}
+                var title = $("<div>");
+                title.addClass("bright medium light");
+                title.html(this.newsItems[this.activeItem+i].title);
+                wrapper.append(title.clone());
 
-		} else {
-			wrapper.innerHTML = this.translate("LOADING");
-			wrapper.className = "small dimmed";
-		}
+                if (this.config.showDescription) {
+                    var description = $("<div>");
+                    description.addClass("small light");
+                    description.html(this.newsItems[this.activeItem+i].description);
+                    wrapper.append(description.clone());
+                }
+            } /* end for loop for number of headlines */
 
-		return wrapper;
-	},
+        } else {
+            wrapper.html(this.translate("LOADING"));
+            wrapper.addClass("small dimmed");
+        }
 
-	/* registerFeeds()
-	 * registers the feeds to be used by the backend.
-	 */
+        return wrapper.get(0);
+    },
 
-	registerFeeds: function() {
-		for (var f in this.config.feeds) {
-			var feed = this.config.feeds[f];
-			this.sendSocketNotification("ADD_FEED", {
-				feed: feed,
-				config: this.config
-			});
-		}
-	},
+        /* registerFeeds()
+         * registers the feeds to be used by the backend.
+         */
 
-	/* registerFeeds()
-	 * Generate an ordered list of items for this configured module.
-	 *
-	 * attribute feeds object - An object with feeds returned by the nod helper.
-	 */
-	generateFeed: function(feeds) {
-		var newsItems = [];
-		for (var feed in feeds) {
-			var feedItems = feeds[feed];
-			if (this.subscribedToFeed(feed)) {
-				for (var i in feedItems) {
-					var item = feedItems[i];
-					item.sourceTitle = this.titleForFeed(feed);
-					newsItems.push(item);
-				}
-			}
-		}
-		newsItems.sort(function(a,b) {
-			var dateA = new Date(a.pubdate);
-			var dateB = new Date(b.pubdate);
-			return dateB - dateA;
-		});
-		if(this.config.maxNewsItems > 0) {
-			newsItems = newsItems.slice(0, this.config.maxNewsItems);
-		}
-		this.newsItems = newsItems;
-	},
+    registerFeeds: function() {
+        for (var f in this.config.feeds) {
+            var feed = this.config.feeds[f];
+            this.sendSocketNotification("ADD_FEED", {
+                feed: feed,
+                config: this.config
+            });
+        }
+    },
 
-	/* subscribedToFeed(feedUrl)
-	 * Check if this module is configured to show this feed.
-	 *
-	 * attribute feedUrl string - Url of the feed to check.
-	 *
-	 * returns bool
-	 */
-	subscribedToFeed: function(feedUrl) {
-		for (var f in this.config.feeds) {
-			var feed = this.config.feeds[f];
-			if (feed.url === feedUrl) {
-				return true;
-			}
-		}
-		return false;
-	},
+        /* registerFeeds()
+         * Generate an ordered list of items for this configured module.
+         *
+         * attribute feeds object - An object with feeds returned by the nod helper.
+         */
+    generateFeed: function(feeds) {
+        var newsItems = [];
+        for (var feed in feeds) {
+            var feedItems = feeds[feed];
+            if (this.subscribedToFeed(feed)) {
+                for (var i in feedItems) {
+                    var item = feedItems[i];
+                    item.sourceTitle = this.titleForFeed(feed);
+                    newsItems.push(item);
+                }
+            }
+        }
+        newsItems.sort(function(a,b) {
+            var dateA = new Date(a.pubdate);
+            var dateB = new Date(b.pubdate);
+            return dateB - dateA;
+        });
+        if(this.config.maxNewsItems > 0) {
+            newsItems = newsItems.slice(0, this.config.maxNewsItems);
+        }
+        this.newsItems = newsItems;
+    },
 
-	/* subscribedToFeed(feedUrl)
-	 * Returns title for a specific feed Url.
-	 *
-	 * attribute feedUrl string - Url of the feed to check.
-	 *
-	 * returns string
-	 */
-	titleForFeed: function(feedUrl) {
-		for (var f in this.config.feeds) {
-			var feed = this.config.feeds[f];
-			if (feed.url === feedUrl) {
-				return feed.title || "";
-			}
-		}
-		return "";
-	},
+        /* subscribedToFeed(feedUrl)
+         * Check if this module is configured to show this feed.
+         *
+         * attribute feedUrl string - Url of the feed to check.
+         *
+         * returns bool
+         */
+    subscribedToFeed: function(feedUrl) {
+        for (var f in this.config.feeds) {
+            var feed = this.config.feeds[f];
+            if (feed.url === feedUrl) {
+                return true;
+            }
+        }
+        return false;
+    },
 
-	/* scheduleUpdateInterval()
-	 * Schedule visual update.
-	 */
-	scheduleUpdateInterval: function() {
-		var self = this;
+        /* subscribedToFeed(feedUrl)
+         * Returns title for a specific feed Url.
+         *
+         * attribute feedUrl string - Url of the feed to check.
+         *
+         * returns string
+         */
+    titleForFeed: function(feedUrl) {
+        for (var f in this.config.feeds) {
+            var feed = this.config.feeds[f];
+            if (feed.url === feedUrl) {
+                return feed.title || "";
+            }
+        }
+        return "";
+    },
 
-		self.updateDom(self.config.animationSpeed);
+        /* scheduleUpdateInterval()
+         * Schedule visual update.
+         */
+    scheduleUpdateInterval: function() {
+        var self = this;
 
-		setInterval(function() {
-			self.activeItem++;
-			self.updateDom(self.config.animationSpeed);
-		}, this.config.updateInterval);
-	},
+        self.updateDom(self.config.animationSpeed);
 
-	/* capitalizeFirstLetter(string)
-	 * Capitalizes the first character of a string.
-	 *
-	 * argument string string - Input string.
-	 *
-	 * return string - Capitalized output string.
-	 */
-	capitalizeFirstLetter: function(string) {
-		return string.charAt(0).toUpperCase() + string.slice(1);
-	},
+        setInterval(function() {
+            self.activeItem += self.config.maxHeadlinesDisplayed;
+            self.updateDom(self.config.animationSpeed);
+        }, this.config.updateInterval);
+    },
+
+        /* capitalizeFirstLetter(string)
+         * Capitalizes the first character of a string.
+         *
+         * argument string string - Input string.
+         *
+         * return string - Capitalized output string.
+         */
+    capitalizeFirstLetter: function(string) {
+        return string.charAt(0).toUpperCase() + string.slice(1);
+    },
 
 
 });
